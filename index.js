d30f3951452d1b28a4c7e08621c4977670759e37
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-03-24 15:39:09 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+
+// image function
+function newImage (url, left, bottom) {
+    let newCharacter = document.createElement('img')
+    newCharacter.src = url
+    newCharacter.style.position = 'fixed'
+    newCharacter.style.left = left + 'px'
+    newCharacter.style.bottom = bottom + 'px'
+    document.body.append(newCharacter)
+    return newCharacter
+}
+
+// object function
+function newItem (url, left, bottom) {
+    let object = newImage(url, left, bottom)
+
+    object.addEventListener('dblclick', function() {
+        object.remove()
+    })
+}
+
+// adding characters//
+newImage ('assets/green-character.gif', 100, 100)
+newImage ('assets/pine-tree.png', 450, 200)
+newImage ('assets/tree.png', 200, 300)
+newImage ('assets/pillar.png', 350, 100)
+newImage ('assets/crate.png', 150, 200)
+newImage ('assets/well.png', 500, 425)
+
+// adding items//
+newItem ('assets/sword.png', 500, 405)
+newItem ('assets/sheild.png', 165, 185)
+newItem ('assets/staff.png', 600, 100)
